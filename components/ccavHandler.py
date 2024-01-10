047f7acffe4a18e69bf7288e62441a94bec74cb8
Arjun Gadvi <arjun.gadvi@gmail.com>
2024-01-10 23:55:36 +0530

    new pay

--- components/ccavHandler.py
+++ components/ccavHandler.py
@@ -5,6 +5,7 @@ from string import Template
 #from Crypto.Util.Padding import pad, unpad
 import hashlib
 from binascii import hexlify, unhexlify
+from pay_ccavenue import CCAvenue
 
 def res(encResp):
     '''Please put in the 32 bit alphanumeric key in quotes provided by CCAvenues.'''
@@ -34,30 +35,71 @@ def res(encResp):
     return fin
 
 
-def pad(data):
-    length = 16 - (len(data) % 16)
-    data += chr(length)*length
-    return data
+# def pad(data):
+#     length = 16 - (len(data) % 16)
+#     data += chr(length)*length
+#     return data
 
-def unpad(data):
-    return data[0:-ord(data[-1])] 
+# def unpad(data):
+#     return data[0:-ord(data[-1])] 
+
+# def encrypt(plainText, workingKey):
+#     iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'.encode("utf-8")
+#     plainText = pad(plainText)
+#     bytearrayWorkingKey = bytearray()
+#     bytearrayWorkingKey.extend(map(ord, workingKey))
+#     enc_cipher = AES.new(hashlib.md5(bytearrayWorkingKey).digest(), AES.MODE_CBC, iv)
+#     return hexlify(enc_cipher.encrypt(plainText.encode("utf-8"))).decode('utf-8')
+
+# def decrypt(cipherText, workingKey):
+#     iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'.encode("utf-8")
+#     encryptedText = unhexlify(cipherText)
+#     bytearrayWorkingKey = bytearray()
+#     bytearrayWorkingKey.extend(map(ord, workingKey))
+#     decCipher = AES.new(hashlib.md5(bytearrayWorkingKey).digest(), AES.MODE_CBC, iv)
+#     return unpad(decCipher.decrypt(encryptedText).decode('utf-8'))
+
+ccavenue = CCAvenue("97DC997FD024D2081D32B75072CAB101", "AVAN40KL46BB14NABB", "3098154", "https://catl.onrender.com/api/ccav/ResponseHandler", "https://catl.onrender.com/api/ccav/ResponseHandler")
 
 def encrypt(plainText, workingKey):
-    iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'.encode("utf-8")
-    plainText = pad(plainText)
-    bytearrayWorkingKey = bytearray()
-    bytearrayWorkingKey.extend(map(ord, workingKey))
-    enc_cipher = AES.new(hashlib.md5(bytearrayWorkingKey).digest(), AES.MODE_CBC, iv)
-    return hexlify(enc_cipher.encrypt(plainText.encode("utf-8"))).decode('utf-8')
+    return ccavenue.encrypt(plainText)
 
 def decrypt(cipherText, workingKey):
-    iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'.encode("utf-8")
-    encryptedText = unhexlify(cipherText)
-    bytearrayWorkingKey = bytearray()
-    bytearrayWorkingKey.extend(map(ord, workingKey))
-    decCipher = AES.new(hashlib.md5(bytearrayWorkingKey).digest(), AES.MODE_CBC, iv)
-    return unpad(decCipher.decrypt(encryptedText).decode('utf-8'))
+    return ccavenue.decrypt(cipherText)
 
-e = encrypt("hello", "hi")
+d = {
+   "merchant_id":"3098154",
+   "order_id":"123456",
+   "currency":"INR",
+   "amount":"1.00",
+   "redirect_url":"https://catl.onrender.com/api/ccav/ResponseHandler",
+   "cancel_url":"https://catl.onrender.com/api/ccav/ResponseHandler",
+   "language":"EN",
+   "billing_name":"Peter",
+   "billing_address":"Santacruz",
+   "billing_city":"Mumbai",
+   "billing_state":"MH",
+   "billing_zip":"400054",
+   "billing_country":"India",
+   "billing_tel":"0229874789",
+   "billing_email":"testing@domain.com",
+   "delivery_name":"Sam",
+   "delivery_address":"Vile Parle",
+   "delivery_city":"Mumbai",
+   "delivery_state":"Maharashtra",
+   "delivery_zip":"400038",
+   "delivery_country":"India",
+   "delivery_tel":"0221234321",
+   "merchant_param1":"additional Info.",
+   "merchant_param2":"additional Info.",
+   "merchant_param3":"additional Info.",
+   "merchant_param4":"additional Info.",
+   "merchant_param5":"additional Info.",
+   "integration_type":"iframe_normal",
+   "promo_code":"",
+   "customer_identifier":""
+}
+
+e = encrypt(d, "hi")
 print(e)
-print(decrypt(e, "hi"))
+print(decrypt({"encResp":"d378150482578dff21c054426a1bf38b61c7411456c40ac9532f843dbbf3413121340fcbf17d238d2fbc572ae6e21eb57e39197da01e161abeaf98e2dd350fd3451856e1cda8fe85d724297a2cbb4180a18f7af0d9dc665c94ad0552389964f28e7647c0b69f2373ba68d7f9ff6ec14ee2b4447f7221ee23eb89951221154d16251e33ce4058f63f147bc0c8533b17c4d4254087a2f5dca10305e3e3f17ef04a36a87314e3bc500c4b963a2ba8f385cb8773f55e9798eaf50a2702accdc619d3ad555b6010508d657c25a52385e74af53283b3ee26beb1f83a5e5610e4a512e6dc414f396734cd5eb779ecf8ac4a0f93ebabe3f5f23b28fbf5a02f8763ce7750fe0abcd75cbb3f9f293f8f958ea09892fe66bc471c49fd8019ee34c73bb8cf5e5e811373f00cbcdf0d786f68ba6c68ad887931b34e1ef8343db48baa5c99068d7889021cd69d1f113e1c4e261e6a426969ec80071105645ebcbdcf517d6eef0a459c204c78f4829d1c011658f6251b7eb68bee4b37043bf3520583f54507c9e69590f44a06b1aa90632cb00d2e967fa54770761a0e6dbd8b9e1838cbf648d6fb49945cee48566f5a32a1fbe0cca74408234779dcf9e5f3037ab97f6d5009a3bf8fe50c78bb7da4a07752c567bb8a161d89f054f5dbe4d90d121e2e5a67282a27440e06e3d42b32fa7c6e209014b92d52f4175845c9cb7081e2b44d74c9487afde9c81e409308bde90ace5087bc5ff2b39642b6515c58f57c2d787fcba21078e346b96ca7e80958981a46ecea1328ad0a80e7fe8b05248014ed4d20c6c556afb2633d1210493a757b8d72e24e9ca8abd5c85a6f4e1c66408210ecfcdb4f7562785f0765273ecb0723776857093b16e6a21cd446ea9103f300e73e95b2af6ddefccc137a6f708f25b346ad81449699f7a4762084fc4a539baa84bb425c30df55dd42c608916dcb572c4120a276978004188de03d38bd00350efa34910b79c1234af0e67ee9470a50d97e8a63f6bbb8ecd831b599bf939fb1c977cc17ce0b5115361e087225ec66d478bd12de7798b58b5c54f8b8a49d368464bc15cea573dde130a85ae56ff9b7f66b6a51c86b17b8793c0a07bc26fc1efa80d825298879024a05"}, "hi"))
--- main.py
+++ main.py
@@ -18,12 +18,6 @@ from routes.ccavMain import ccav_router
 from components.commonutils import SendMail
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
-from fastapi.templating import Jinja2Templates
-from fastapi import Request
-from string import Template
-from fastapi.responses import HTMLResponse
-from components.ccavHandler import res, encrypt
-
 otpCode = 0
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
@@ -50,7 +44,7 @@ from routes import Token, User, UserModel, UserInDB, models, authenticapi
 from routes import OAuth2PasswordRequestForm, ACCESS_TOKEN_EXPIRE_MINUTES
 
 
-@app.get("/app", response_class=RedirectResponse)
+@app.get("/", response_class=RedirectResponse)
 async def mainapp():
     return "/static/index.html"
 
@@ -154,83 +148,3 @@ async def read_users_me(current_user: str = Depends(authenticapi.get_current_act
 # if __name__ == '__main__':
 #     port = int(getenv("PORT", 8000))
 #     uvicorn.run("main:app",port=port, host='0.0.0.0', reload = True, reload_dirs = ["html_files"])
-
-
-@app.get('/', response_class=HTMLResponse)
-async def webpay(request: Request):
-    return templates.TemplateResponse("dataFrom.htm", {"request": request})
-
-
-@app.post('/Response')
-async def payResponse(request: Request):
-    plainText = res(request.form().get('encResp'))
-    print("res: ", plainText)
-    return plainText
-
-
-@app.post('/ReqestHandler', response_class=HTMLResponse)
-async def payRequest(request: Request):
-    req = await request.form()
-    p_merchant_id = req.get('merchant_id')
-    p_order_id = req.get('order_id')
-    p_currency = req.get('currency')
-    p_amount = req.get('amount')
-    p_redirect_url = req.get('redirect_url')
-    p_cancel_url = req.get('cancel_url')
-    p_language = req.get('language')
-    p_billing_name = req.get('billing_name')
-    p_billing_address = req.get('billing_address')
-    p_billing_city = req.get('billing_city')
-    p_billing_state = req.get('billing_state')
-    p_billing_zip = req.get('billing_zip')
-    p_billing_country = req.get('billing_country')
-    p_billing_tel = req.get('billing_tel')
-    p_billing_email = req.get('billing_email')
-    p_delivery_name = req.get('delivery_name')
-    p_delivery_address = req.get('delivery_address')
-    p_delivery_city = req.get('delivery_city')
-    p_delivery_state = req.get('delivery_state')
-    p_delivery_zip = req.get('delivery_zip')
-    p_delivery_country = req.get('delivery_country')
-    p_delivery_tel = req.get('delivery_tel')
-    p_merchant_param1 = req.get('merchant_param1')
-    p_merchant_param2 = req.get('merchant_param2')
-    p_merchant_param3 = req.get('merchant_param3')
-    p_merchant_param4 = req.get('merchant_param4')
-    p_merchant_param5 = req.get('merchant_param5')
-    p_integration_type = req.get('integration_type')
-    p_promo_code = req.get('promo_code')
-    p_customer_identifier = req.get('customer_identifier')
-
-    merchant_data = 'merchant_id=' + p_merchant_id + '&' + 'order_id=' + p_order_id + '&' + "currency=" + p_currency + '&' + 'amount=' + p_amount + '&' + 'redirect_url=' + p_redirect_url + '&' + 'cancel_url=' + p_cancel_url + '&' + 'language=' + p_language + '&' + 'billing_name=' + p_billing_name + '&' + 'billing_address=' + p_billing_address + '&' + 'billing_city=' + p_billing_city + '&' + 'billing_state=' + p_billing_state + '&' + 'billing_zip=' + p_billing_zip + '&' + 'billing_country=' + p_billing_country + '&' + 'billing_tel=' + p_billing_tel + '&' + 'billing_email=' + p_billing_email + '&' + 'delivery_name=' + p_delivery_name + '&' + 'delivery_address=' + p_delivery_address + '&' + 'delivery_city=' + p_delivery_city + '&' + 'delivery_state=' + p_delivery_state + '&' + 'delivery_zip=' + p_delivery_zip + '&' + 'delivery_country=' + p_delivery_country + '&' + 'delivery_tel=' + p_delivery_tel + '&' + 'merchant_param1=' + p_merchant_param1 + '&' + 'merchant_param2=' + p_merchant_param2 + '&' + 'merchant_param3=' + p_merchant_param3 + '&' + 'merchant_param4=' + p_merchant_param4 + '&' + 'merchant_param5=' + p_merchant_param5 + '&' + 'integration_type=' + p_integration_type + '&' + 'promo_code=' + p_promo_code + '&' + 'customer_identifier=' + p_customer_identifier + '&'
-    # print("mer:", merchant_data)
-    encryption = encrypt(merchant_data, workingKey)
-
-    html = '''\
-        <html>
-        <head>
-            <title>Sub-merchant checkout page</title>
-            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        </head>
-        <body>
-            <center>
-            <!-- width required mininmum 482px -->
-                <iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id=$mid&encRequest=$encReq&access_code=$xscode">
-                </iframe>
-            </center>
-            
-            <script type="text/javascript">
-                $(document).ready(function(){
-                    $('iframe#paymentFrame').load(function() {
-                        window.addEventListener('message', function(e) {
-                            $("#paymentFrame").css("height",e.data['newHeight']+'px'); 	 
-                        }, false);
-                    }); 
-                });
-            </script>
-        </body>
-        </html>
-        '''
-    fin = Template(html).safe_substitute(mid=p_merchant_id,encReq=encryption,xscode=accessCode)
-    return fin
-    # return templates.TemplateResponse('merPage.htm', {"request":req, "mid":p_merchant_id,"encReq":merchant_data, "xscode":accessCode})
