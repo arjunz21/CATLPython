39b49871121a1edea7f61d40a5ef2a3f43de9228
Arjun Gadvi <arjun.gadvi@gmail.com>
2024-01-10 16:52:35 +0530

    new pay

--- components/ccavHandler.py
+++ components/ccavHandler.py
@@ -32,31 +32,33 @@ def res(encResp):
     return fin
 
 
-# def pad(data):
-#     length = 16 - (len(data) % 16)
-#     data += chr(length) * length
-#     return data
+def pade(data):
+    length = 16 - (len(data) % 16)
+    data += chr(length) * length
+    return data
 
 
 def encrypt(plainText, workingKey):
     iv = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
+    #plainText = pade(plainText)
     plainText = pad(plainText.encode('utf-8'), AES.block_size)
     encDigest = md5()
     encDigest.update(workingKey.encode())
     enc_cipher = AES.new(encDigest.digest(), AES.MODE_CBC, iv)
     encryptedText = enc_cipher.encrypt(plainText)
-    return encryptedText
+    return encryptedText.hex()
 
 
 def decrypt(cipherText, workingKey):
     iv = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
     decDigest = md5()
     decDigest.update(workingKey.encode())
-    #encryptedText = cipherText.decode('hex')
+    encryptedText = bytes.fromhex(cipherText)
     dec_cipher = AES.new(decDigest.digest(), AES.MODE_CBC, iv)
-    decryptedText = unpad(dec_cipher.decrypt(cipherText), AES.block_size)
-    return decryptedText
+    decryptedText = unpad(dec_cipher.decrypt(encryptedText), AES.block_size)
+    return decryptedText.decode('utf-8')
 
 e = encrypt("hello", "hi")
 print(e)
 print(decrypt(e, "hi"))
+#104996c0e0cbf620bc8762f1d362d0b6
