7c10e133e1ebf6efd6103dfe658e833fe78bf01e
Arjun Gadvi <arjun.gadvi@gmail.com>
2024-01-10 18:02:51 +0530

    new pay

--- components/ccavHandler.py
+++ components/ccavHandler.py
@@ -40,12 +40,12 @@ def pade(data):
 
 def encrypt(plainText, workingKey):
     iv = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
-    #plainText = pade(plainText)
-    plainText = pad(plainText.encode('utf-8'), AES.block_size)
+    plainText = pade(plainText)
+    #plainText = pad(plainText.encode('utf-8'), AES.block_size)
     encDigest = md5()
     encDigest.update(workingKey.encode())
     enc_cipher = AES.new(encDigest.digest(), AES.MODE_CBC, iv)
-    encryptedText = enc_cipher.encrypt(plainText)
+    encryptedText = enc_cipher.encrypt(plainText.encode())
     return encryptedText.hex()
 
 
